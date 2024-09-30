73be2317acdb984aa6c3f69965320eb9b7c001d6
Tameratlant <80047396+Tameratlant@users.noreply.github.com>
2024-09-30 18:25:59 +0300

    Update main.cpp

--- FirstTask/main.cpp
+++ FirstTask/main.cpp
@@ -1,11 +1,4 @@
-#include <QApplication>
-#include <QWidget>
-#include <QPushButton>
-#include <QLineEdit>
-#include <QVBoxLayout>
-#include <QDateTime>
-#include <QFile>
-#include <QTextStream>
+
 #include "mainwindow.cpp"
 
 int main(int argc, char *argv[]) {
